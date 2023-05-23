7e58002bd99cef864a012ad13ddf5f93a2fef96f
emrahhakanagan <emrahhakanagan@gmail.com>
2023-05-23 06:43:08 +0300

    Yandex exercise Cosmic Modul has been uptaded

---
+++ src/yandex/practicum/space/station/Zvezda.java
@@ -0,0 +1,19 @@
+package yandex.practicum.space.station;
+
+class Zvezda extends Zarya {
+
+    public Zvezda() { // доступ к изменению конструктора запрещён
+        super();
+        energyConsumption = 44;
+        activateModule();
+    }
+    // доступ к созданию новых конструкторов запрещён
+
+    // разрешено переопределение методов
+    @Override
+    void activateModule() {
+        turnSolarPanels();
+    }
+
+
+}
